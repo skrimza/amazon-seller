141d5cbfc24c7a7d7bade9fc083892fe2dd16a86
Ilya <112574854+skrimza@users.noreply.github.com>
2024-06-02 01:05:21 +0300

    add scroll animation website

--- static/js/main.js
+++ static/js/main.js
@@ -21,6 +21,16 @@ $(function () {
     });
 });
 
+$(function () {
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+});
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
