83bc97b468de7d55e10d01324fe0fc4e4b544937
Ilya <112574854+skrimza@users.noreply.github.com>
2024-06-28 10:33:05 +0300

    update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def send_email(subject, body, recipient):
     msg.attach(MIMEText(body, 'plain', 'utf-8'))
     try:
         with smtplib.SMTP( host=settings.MAIL_SERVER, port=settings.MAIL_PORT) as server:
-            server.ehlo()
+            # server.ehlo()
             server.starttls()
             server.login(settings.MAIL_USERNAME, settings.MAIL_PASSWORD)
             server.auth_plain()
