38271b94be6884e92cf2d3e3791204829438a0a5
Ilya <112574854+skrimza@users.noreply.github.com>
2024-07-01 01:39:10 +0300

    update app.py

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ app.config['ID_OWNER'] = settings.ID_OWNER.get_secret_value()
 app.config['BOT_TOKEN'] = settings.BOT_TOKEN.get_secret_value()
 
 
-def send_message_telegram(chat_id, text):
+def send_message_telegram(text):
     headers = {'Content-type': 'application/json'}
     payload = {
-        'chat_id': chat_id,
+        'chat_id': settings.ID_OWNER,
         'text': text
     }
     json_data = json.dumps(payload)
@@ -40,7 +40,7 @@ def send_message():
     if isinstance(result, dict):
         owner_data_body = (f"New request from the website:\n Name: {result.get('username')},\n email: ({result.get('email')}):\n\n"
                             f"text: {result.get('message')}")
-        response = send_message_telegram(settings.ID_OWNER.get_secret_value(), owner_data_body)        
+        response = send_message_telegram(owner_data_body)        
         return jsonify(response)
     else:
         return result
