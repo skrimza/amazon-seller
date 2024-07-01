2e713e2903462fb0db21c34df8b4cbc7d8a945bb
Ilya <112574854+skrimza@users.noreply.github.com>
2024-07-01 03:21:54 +0300

    update app.py

--- app.py
+++ app.py
@@ -13,6 +13,25 @@ app.config['ID_OWNER'] = settings.ID_OWNER
 app.config['BOT_TOKEN'] = settings.BOT_TOKEN.get_secret_value()
 
 
+def get_updates():
+    try:
+        conn = client.HTTPSConnection("api.telegram.org")
+        conn.request(HTTPMethod.GET, f"/bot{settings.BOT_TOKEN}/getUpdates")
+        response = conn.getresponse()
+        
+        if response.status == 200:
+            data = response.read()
+            updates = json.loads(data)
+            conn.close()
+            return updates
+        else:
+            conn.close()
+            return {"error": f"Failed to fetch updates. Status code: {response.status}"}
+    
+    except Exception as e:
+        return {"error": f"Exception occurred: {str(e)}"}
+    
+
 def send_message_telegram(text):
     headers = {'Content-type': 'application/json'}
     payload = {
@@ -22,7 +41,6 @@ def send_message_telegram(text):
     json_data = json.dumps(payload)
     conn = client.HTTPSConnection("api.telegram.org")
     conn.request(HTTPMethod.POST, f"/bot{settings.BOT_TOKEN}/sendMessage", json_data, headers=headers)
-    
     response = conn.getresponse()
     data = response.read()
     
@@ -37,6 +55,7 @@ def homepage():
 
 @app.route(rule="/contact", methods=[HTTPMethod.POST])
 def send_message():
+    updates = get_updates()
     result = pyform(data=request.form.to_dict())
     if isinstance(result, dict):
         owner_data_body = (f"New request from the website:\n Name: {result.get('username')},\n email: ({result.get('email')}):\n\n"
