399bebd580e7b606067ec6640a64b286abb65e03
Ilya <112574854+skrimza@users.noreply.github.com>
2024-07-02 03:14:03 +0300

    update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def send_message():
     if isinstance(result, dict):
         owner_data_body = (f"New request from the website:\n Name: {result.get('username')},\n email: ({result.get('email')}):\n\n"
                             f"text: {result.get('message')}")
-        response = send_message_telegram(parse.quote(owner_data_body))        
+        response = send_message_telegram(owner_data_body.replace('\n', ' '))        
         return jsonify(response)
     else:
         return result
